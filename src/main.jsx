f4cc0e0d7fa1087f60e0920297c5b09fca2297ef
taskjionline <taskjionline@gmail.com>
2026-06-27 16:49:49 +0530

    add reffer link

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+// Global Axios Interceptor for CRM API Calls (adds referral code to request body)
+axios.interceptors.request.use((config) => {
+  if (config.url && config.url.includes('crm.doxez.in') && config.method === 'post') {
+    const referralCode = localStorage.getItem('dxz_ref_code');
+    if (referralCode) {
+      config.data = {
+        ...config.data,
+        referralCode: referralCode
+      };
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
